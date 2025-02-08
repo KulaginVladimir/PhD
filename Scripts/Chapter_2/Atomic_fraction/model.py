9b844cb1167d9257caa9f18b603afce30418956d
KulaginVladimir <kulagin.vladimir.l@yandex.ru>
2025-02-08 03:14:45 +0300

    Atomic fraction

---
+++ Scripts/Chapter_2/Atomic_fraction/model.py
@@ -0,0 +1,281 @@
+import festim as F
+import fenics as f
+import numpy as np
+import sympy as sp
+import sub_functions.heat_pulses as heat_pulses
+from sub_functions.surface_kinetics import CustomSurfaceKinetics
+from sub_functions.materials import W, Be
+
+L_tr = 10e-6  # trap layer, m
+trap_distribution = sp.Piecewise((1, F.x < L_tr), (0, True))
+T0 = 300  # initial temperature, K
+
+
+class LID_simulation:
+    def __init__(
+        self,
+        E_laser,
+        duration,
+        material_name,
+        E_dt,
+        eta_tr,
+        phi,
+        output_folder,
+        is_txt=False,
+    ):
+        self.E_laser = E_laser
+        self.duration = duration
+        self.material_name = material_name
+        self.E_dt = E_dt
+        self.eta_tr = eta_tr
+        self.phi = phi
+        self.output_folder = output_folder
+        self.is_txt = is_txt
+
+        if duration == "ns":
+            self.q_heat = lambda t: E_laser * heat_pulses.gauss_heat(t, 40e-9, 25e-9)
+
+            self.final_time = 1000e-9
+            self.step_size = F.Stepsize(
+                initial_value=1e-10,
+                stepsize_change_ratio=1.05,
+                max_stepsize=lambda t: 0.1e-9 if t < 80e-9 else 1e-7,
+                dt_min=1e-11,
+            )
+
+            self.vertices = np.concatenate(
+                [
+                    np.linspace(0, 1e-6, 1000),
+                    np.linspace(1e-6, 10e-6, 500),
+                    np.linspace(10e-6, 100e-6, 500),
+                ]
+            )
+
+        elif duration == "us":
+            self.q_heat = lambda t: E_laser * heat_pulses.trapezoid_heat(
+                t, 2.5e-6, 9.5e-6, 0, 0.5e-6, 0.5e-6
+            )
+
+            self.final_time = 100e-6
+            self.step_size = F.Stepsize(
+                initial_value=1e-8,
+                stepsize_change_ratio=1.05,
+                max_stepsize=lambda t: 1e-8 if t < 30e-6 else 1e-6,
+                dt_min=1e-11,
+            )
+
+            self.vertices = np.concatenate(
+                [
+                    np.linspace(0, 10e-6, 1000),
+                    np.linspace(10e-6, 50e-6, 1000),
+                    np.linspace(50e-6, 0.5e-3, 500),
+                ]
+            )
+
+        elif duration == "ms":
+            self.q_heat = lambda t: E_laser * heat_pulses.trapezoid_heat(
+                t, 0.25e-3, 4.95e-3, 0.3, 0.05e-3, 0.05e-3
+            )
+
+            self.final_time = 20e-3
+            self.step_size = F.Stepsize(
+                initial_value=1e-6,
+                stepsize_change_ratio=1.05,
+                max_stepsize=lambda t: 1e-5 if t < 10e-3 else 5e-5,
+                dt_min=1e-11,
+            )
+
+            self.vertices = np.concatenate(
+                [
+                    np.linspace(0, 10e-6, 1000),
+                    np.linspace(10e-6, 1e-4, 1000),
+                    np.linspace(1e-4, 20e-3, 500),
+                ]
+            )
+
+        if material_name == "Be":
+            self.material = Be()
+        elif material_name == "W":
+            self.material = W()
+        else:
+            raise ValueError(f"Material {material_name} is not defined")
+
+    def exp_type(self, x):
+        if isinstance(x, np.ndarray):
+            return np.exp(x)
+        else:
+            return f.exp(x)
+
+    def check_Qc(self, Qc, cs):
+        if callable(Qc):
+            return Qc(cs)
+        else:
+            return Qc
+
+    def k_sb(self, T, cs, cm):
+        mat = self.material
+        Q_c = self.check_Qc(mat.Q_c, cs)
+
+        return mat.nu_0 * self.exp_type(-(mat.E_s - Q_c) / F.k_B / T)
+
+    def k_bs(self, T, cs, cm):
+        mat = self.material
+
+        return (
+            mat.nu_D * mat.lambda_abs * self.exp_type(-(mat.E_s - mat.Q_s) / F.k_B / T)
+        )
+
+    def J_a(self, T, cs):
+        mat = self.material
+        Q_c = self.check_Qc(mat.Q_c, cs)
+        k_a = mat.nu_0 * self.exp_type(-(mat.E_d - Q_c) / F.k_B / T)
+
+        return cs * k_a
+
+    def J_m_s(self, T, cs):
+        mat = self.material
+        Q_c = self.check_Qc(mat.Q_c, cs)
+        k_m_s = mat.nu_0 / mat.n_surf * self.exp_type(-2 * (mat.E_c - Q_c) / F.k_B / T)
+
+        return 2 * cs**2 * k_m_s
+
+    def J_m_sb(self, T, cs, cm):
+        mat = self.material
+        Q_c = self.check_Qc(mat.Q_c, cs)
+        k_m_sb = (
+            mat.nu_D
+            / mat.n_IS
+            * self.exp_type(-(mat.E_c - Q_c + mat.E_s - mat.Q_s) / F.k_B / T)
+        )
+
+        return cs * cm * k_m_sb
+
+    def J_m_b(self, T, cm):
+        mat = self.material
+
+        k_m_b = (
+            mat.nu_D
+            / mat.n_IS
+            * mat.lambda_abs
+            * self.exp_type(-2 * (mat.E_s - mat.Q_s) / F.k_B / T)
+        )
+
+        return 2 * cm**2 * k_m_b
+
+    def J_vs(self, T, cs, cm):
+        return -self.J_a(T, cs) - self.J_m_s(T, cs) - self.J_m_sb(T, cs, cm)
+
+    def J_vb(self, T, cs, cm):
+        return -self.J_m_b(T, cm) - self.J_m_sb(T, cs, cm)
+
+    def run(self):
+        LID_model = F.Simulation(log_level=40)
+
+        LID_model.mesh = F.MeshFromVertices(self.vertices)
+
+        material = F.Material(
+            id=1,
+            D_0=self.material.D_0,
+            E_D=self.material.E_diff,
+            thermal_cond=self.material.thermal_cond_function,
+            heat_capacity=self.material.heat_capacity_function,
+            rho=self.material.rho,
+            Q=self.material.heat_of_transport_function,
+        )
+
+        LID_model.materials = [material]
+
+        LID_model.traps = [
+            F.Trap(
+                k_0=self.material.nu_D / self.material.n_IS,
+                E_k=self.material.E_diff,
+                p_0=self.material.nu_0,
+                E_p=self.E_dt,
+                density=self.eta_tr * self.material.n_mat * trap_distribution,
+                materials=material,
+            )
+        ]
+
+        LID_model.T = F.HeatTransferProblem(
+            transient=True,
+            initial_condition=T0,
+            absolute_tolerance=1.0,
+            relative_tolerance=1e-3,
+        )
+
+        LID_model.boundary_conditions = [
+            F.DirichletBC(field=0, value=0, surfaces=2),
+            F.DirichletBC(field="T", value=T0, surfaces=2),
+            F.FluxBC(field="T", value=self.q_heat(F.t), surfaces=1),
+            CustomSurfaceKinetics(
+                k_bs=self.k_bs,
+                k_sb=self.k_sb,
+                lambda_IS=self.material.lambda_IS,
+                n_surf=self.material.n_surf,
+                n_IS=self.material.n_IS,
+                J_vs=self.J_vs,
+                J_vb=self.J_vb,
+                initial_condition=0,
+                surfaces=1,
+            ),
+        ]
+
+        LID_model.initial_conditions = [
+            F.InitialCondition(
+                field="1",
+                value=self.phi * self.eta_tr * self.material.n_mat * trap_distribution,
+            )
+        ]
+
+        LID_model.dt = self.step_size
+
+        LID_model.settings = F.Settings(
+            absolute_tolerance=1e15,
+            relative_tolerance=1e-5,
+            final_time=self.final_time,
+            soret=True,
+            traps_element_type="DG",
+        )
+
+        filename_params = f"_{self.material_name}_{self.duration}_E{self.E_laser/1e6:.5f}MJ_Edt{self.E_dt:.5f}eV_eta{-np.log10(self.eta_tr):.5f}"
+        derived_quantities = F.DerivedQuantities(
+            [
+                F.AdsorbedHydrogen(surface=1),
+                F.HydrogenFlux(surface=1),
+                F.TotalSurface(field="T", surface=1),
+                F.PointValue(field=0, x=0),
+            ],
+            show_units=True,
+            filename=f"{self.output_folder}/data{filename_params}.csv",
+        )
+
+        TXT = [
+            F.TXTExport(
+                field="retention",
+                filter=True,
+                write_at_last=True,
+                filename=f"{self.output_folder}/ret{filename_params}.txt",
+            ),
+            F.TXTExport(
+                field="1",
+                filter=True,
+                write_at_last=True,
+                filename=f"{self.output_folder}/trapped{filename_params}.txt",
+            ),
+            F.TXTExport(
+                field="solute",
+                filter=True,
+                write_at_last=True,
+                filename=f"{self.output_folder}/solute{filename_params}.txt",
+            ),
+        ]
+
+        LID_model.exports = [derived_quantities]
+
+        if self.is_txt:
+            LID_model.exports += TXT
+
+        LID_model.initialise()
+        LID_model.run()
+
+        return derived_quantities
---
+++ Scripts/Chapter_2/Atomic_fraction/parametric_study.py
@@ -0,0 +1,51 @@
+from model import LID_simulation
+import sys
+import numpy as np
+
+N = int(sys.argv[1])
+
+E_dt = 1.5
+eta_tr = 1e-2
+phi = 1
+
+materials = ["W", "Be"]
+durations = ["ns", "us", "ms"]
+E_maxs = [10.2e3, 160e3, 3.8e6]
+
+prms = []
+for mat in materials:
+    for i in range(3):
+        prms.append([E_maxs[i], durations[i], mat])
+
+for i in range(1, 26):
+    E_max, dur, mat = prms[N - 1]
+
+    model = LID_simulation(
+        E_max * i / 25,
+        dur,
+        mat,
+        E_dt,
+        eta_tr,
+        phi,
+        "../results",
+        is_txt=False,
+    )
+
+    results = model.run()
+
+    t = np.array(results.t)
+    cs = np.array(results[0].data)
+    T = np.array(results[2].data)
+    cm = np.array(results[3].data)
+
+    Jat = model.J_a(T, cs)
+    Jmol_s = model.J_m_s(T, cs)
+    J_mol_sb = 2 * model.J_m_sb(T, cs, cm)
+    J_mol_b = model.J_m_b(T, cm)
+
+    J_des = Jat + Jmol_s + J_mol_b + J_mol_sb
+
+    out = np.column_stack([Jat, Jmol_s, J_mol_sb, J_mol_b, J_des])
+
+    filename = f"../results/fluxes_{mat}_{dur}_E{E_max/1e6:.5f}MJ_Edt{E_dt:.5f}eV_eta{-np.log10(eta_tr):.5f}.csv"
+    np.savetxt(filename, out, delimiter=",")
